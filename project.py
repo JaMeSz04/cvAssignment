97d86770e469ecffab6a281334e394d950397114
wiranchana <wiranchana@users.noreply.github.com>
2017-11-13 19:22:07 +0700

    Update segmentation

--- project.py
+++ project.py
@@ -2,39 +2,77 @@
 import cv2
 import numpy as np
 
-def distanceThresholding(colorImg, color, T):
-    rows, cols ,chs= colorImg.shape
-    outImg = np.zeros((rows, cols), np.uint8)
-    outColorImg = np.zeros(colorImg.shape, np.uint8)
-    for r in range(rows):
-        for c in range(cols):
-            d = colorImg[r,c,:] - color[:]
-            dist = np.sqrt(d[0]*d[0]+d[1]*d[1]+d[2]*d[2])
-            if dist < T:
-                outImg[r,c] = 255
-                outColorImg[r,c,:] = colorImg[r,c,:]
-    return outImg, outColorImg
-
 img = cv2.imread("/Users/wiranchana/Desktop/kmitl/S1Y4/cv/FP_DB/1_1.BMP",cv2.IMREAD_GRAYSCALE)
 #img = cv2.imread("/Users/wiranchana/Desktop/kmitl/S1Y4/cv/FP_DB2/101_1.tif",cv2.IMREAD_GRAYSCALE)
-#img = cv2.resize(img, (500, 500)) 
 cv2.imshow("input image",img)
 
 retval, segmentedImg = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
 
-#outImg , outColorImg= distanceThresholding(img,(0,255,0),200)
-#print("Otsu's threshold = ", retval)
+#cv2.imshow('seg',segmentedImg)
+
+row,col = segmentedImg.shape
+lb,tb,rb,bb = False,False,False,False
 
 
-cv2.imshow('seg',segmentedImg)
+#check if left border contain finger
+for r in range(row):
+    if(segmentedImg[r,0]==255):
+        lb = True
+        
+#check if right border contain finger
+for r in range(row):
+    if(segmentedImg[r,col-1]==255):
+        rb = True
 
-invertedImg  = cv2.bitwise_not(segmentedImg)
-cv2.imshow('negative',invertedImg)
+#check if top border contain finger
+for c in range(col):
+    if(segmentedImg[0,c]==255):
+        tb = True
+        
+#check if bottom border contain finger
+for c in range(col):
+    if(segmentedImg[row-1,c]==255):
+        bb = True
+        
 
-highpass = np.ones((3,3), np.int8) * -1
-highpass[1,1] = 8
+# from left border
+if (not lb):
+    for r in (range(row)):
+        for c in range(col):
+            if(segmentedImg[r,c]==255):
+                break
+            else:
+                segmentedImg[r,c] =255
+    
 
-outImg = cv2.filter2D(invertedImg, cv2.CV_8U, highpass)
-#cv2.imshow('highpass',outImg)
+#from right border
+if (not rb):
+    for r in range(row-1,0,-1):
+        for c in range(col-1,0,-1):
+            if(segmentedImg[r,c]==255):
+                break
+            else:
+                segmentedImg[r,c] =255
 
+
+#from top border
+if (not tb):
+    for c in range(col):
+        for r in range(row):
+            if(segmentedImg[r,c]==255):
+                break
+            else:
+                segmentedImg[r,c] =255
+
+
+#from botton border
+if(not bb):
+    for c in range(col-1,0,-1):
+        for r in range(row-1,0,-1):
+            if(segmentedImg[r,c]==255):
+                break
+            else:
+                segmentedImg[r,c] =255
+
+cv2.imshow('Output',segmentedImg)
 cv2.waitKey()
